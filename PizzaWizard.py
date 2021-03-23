75645b9462f4e761d90e9d6ec74fbca88b39ea8a
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2021-03-23 08:47:28 -0700

    Lilja stuuff

--- PizzaWizard.py
+++ PizzaWizard.py
@@ -1,6 +1,6 @@
 import pygame, sys, time
 from pygame.locals import *
-#This class will contain all variables for the wizard
+
 class Wizard:
 	def __init__(self, canvas):
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
